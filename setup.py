46a8fa1b34ab9a59f072434708caddedd65b0f8d
Viktor Persson <vph.persson@gmail.com>
2019-12-22 21:44:58 +0100

    Added dependency links.

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ setup(
     name='smb',
     version='0.1',
     packages=find_packages(),
+    dependency_links=[
+        'git+https://github.com/vphpersson/msdsalgs#egg=msdsalgs',
+        'git+https://github.com/vphpersson/ntlm#egg=ntlm',
+        'git+https://github.com/vphpersson/spnego#egg=spnego',
+        'git+https://github.com/vphpersson/asn1#egg=asn1'
+    ]
 )

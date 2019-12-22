cdb2ef41a56164057c214a1158ddcea1d8d84cd7
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2019-12-22 22:12:12 +0100

    Trying to fix setup.py.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ setup(
     name='smb',
     version='0.1',
     packages=find_packages(),
-    dependency_links=[
-        'http://github.com/vphpersson/msdsalgs/tarball/master#egg=msdsalgs',
-        'http://github.com/vphpersson/ntlm/tarball/master#egg=ntlm',
-        'http://github.com/vphpersson/spnego/tarball/master#egg=spnego',
-        'http://github.com/vphpersson/asn1/tarball/master#egg=asn1'
+    install_requires=[
+        'msdsalgs @ http://github.com/vphpersson/msdsalgs/tarball/master',
+        'ntlm @ http://github.com/vphpersson/ntlm/tarball/master',
+        'spnego @ http://github.com/vphpersson/spnego/tarball/master',
+        'asn1 @ http://github.com/vphpersson/asn1/tarball/master'
     ]
 )

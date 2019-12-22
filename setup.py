793cb5c64e0b6b17ac0a1a04c353060e6a2fc1da
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2019-12-22 21:57:36 +0100

    Trying to fix setup.py again.

--- setup.py
+++ setup.py
@@ -8,11 +8,5 @@ setup(
         'http://github.com/vphpersson/ntlm/tarball/master#egg=ntlm',
         'http://github.com/vphpersson/spnego/tarball/master#egg=spnego',
         'http://github.com/vphpersson/asn1/tarball/master#egg=asn1'
-    ],
-    install_requires=[
-        'msdsalgs',
-        'ntlm',
-        'spnego',
-        'asn1'
-    ],
+    ]
 )

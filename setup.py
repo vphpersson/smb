1d3f229655f81ca4b59307efffa445337664a77d
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2022-08-04 01:07:54 +0200

    Fix references.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='smb',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     install_requires=[
-        'msdsalgs @ git+ssh://git@github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
-        'ntlm @ git+ssh://git@github.com/vphpersson/ntlm.git#egg=ntlm',
-        'spnego @ git+ssh://git@github.com/vphpersson/spnego.git#egg=spnego',
-        'asn1 @ git+ssh://git@github.com/vphpersson/asn1.git#egg=asn1'
+        'msdsalgs @ git+https://github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
+        'ntlm @ git+https://github.com/vphpersson/ntlm.git#egg=ntlm',
+        'spnego @ git+https://github.com/vphpersson/spnego.git#egg=spnego',
+        'asn1 @ git+https://github.com/vphpersson/asn1.git#egg=asn1'
     ]
 )
